d8c37d520ab365b0e0ec3a993beab6dcc49c8245
Florian Miedtke <FlorianMiedtke@users.noreply.github.com>
2025-03-03 10:20:41 +0100

    Revert Mult.asm back to simple version. Was accidentaly overridden.

--- projects/04/Mult.asm
+++ projects/04/Mult.asm
@@ -10,20 +10,12 @@
 
 //Pseudo code:
 
-//if(R0 < R1){
-//	factorsmall = R0
-//  factorbig = R1
-//} else {
-//	factorsmall = R1
-//  factorbig = R0
-//}
-//
 //Loop
 //i = 0
 //(LOOP)
-//	if(i > factorsmall) GOTO END
+//	if(i > R0) GOTO END
 //
-//	sum = sum + factorbig
+//	sum = sum + R1
 //
 //	i++
 //
@@ -32,46 +24,11 @@
 //(END)
 //	JUMP END
 
-//Determine which factor is bigger
-	@R0
-	D=M
-	@R1
-	A=M
-	D=D-A
-
-//if R1 < R0 JMP
-	@R1SMALLER
-	D;JGT
-
-//else
-	@R0
-	D=M
-	@factorsmall
-	M=D
-	@R1
-	D=M
-	@factorbig
-	M=D
-
-	@LOOPINIT
-	0;JMP
-
-(R1SMALLER)
-	@R1
-	D=M
-	@factorsmall
-	M=D
-	@R0
-	D=M
-	@factorbig
-	M=D
-
-(LOOPINIT)
-	@i
-	M=0
+@i
+M=0
 
 (LOOP)
-	@factorsmall
+	@R0
 	D=M
 	@i
 	A=M
@@ -79,7 +36,7 @@
 	@END
 	D;JLE
 	
-	@factorbig
+	@R1
 	D=M
 	@R2
 	M=D+M
